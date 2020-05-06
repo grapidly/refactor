fdc4c3aca99ae4f5a03a05763cdf08370be0c97e
riwanski <rwanski@gmail.com>
2020-05-06 00:34:29 -0400

    css saving file getting closer.  still some bugs in regex code.

--- refactor.py
+++ refactor.py
@@ -3,6 +3,7 @@ import re
 
 # =============================== BUGS ===============================
 #       BUG -- 'follow_frame-bottom' class in styles dict but NOT appending suffix.
+#       BUG -- '.one_col_text--header h3' by there --> '.two_col_text__OOO--column-one__OOO,' appending to middle of class.
 
 
 class Refactor:
@@ -14,6 +15,8 @@ class Refactor:
         self.css = None
         self.html = None
         self.js = None
+        self.EXCLUSION_LIST = ['.woff', '.jpg',
+                               '.woff2', '.js-', '.ug-', '.active']
 
     def __repr__(self):
         try:
@@ -24,7 +27,7 @@ class Refactor:
 
       # =============================== UTIL METHODS ===============================
 
-    def __load_website_files(self):
+    def __load_files(self):
         with open('test.css', 'r') as fp:
             self.css = fp.read()
         with open('test.html', 'r') as fp:
@@ -34,8 +37,9 @@ class Refactor:
         return self.css, self.html, self.js
 
     def __load_re_matches(self):
-        css, html, js = self.__load_website_files()
+        css, html, js = self.__load_files()
         css_re = r"""\.\D[^]['?#\s\r\n,{};:/\/\().]+"""
+        css_re2 = r"""\.(?!png|svg|com|url|w3|ttf|org|gif|jpg|jpeg|onlinewebfonts|eot|woff)([a-z-_]+[^-.,)(\/])"""
         html_re = r"""class=\".*\""""
         js_re = r""".*"""
         css_matches = re.finditer(
@@ -47,27 +51,33 @@ class Refactor:
         return css_matches, html_matches, js_matches
 
     def __create_styles_dict(self, matchNum, match):
-        EXCLUSION_LIST = ['.woff', '.jpg', '.woff2', '.js', '.ug', '.active']
         old_value = match.group()
         if len(old_value) < 5:
             pass
-        elif old_value in EXCLUSION_LIST:
+        elif old_value in self.EXCLUSION_LIST:
             pass
         else:
-            old_value = old_value.replace('.', '')
+            # old_value = old_value.replace('.', '')
             new_value = old_value+self.suffix
             self.styles[old_value] = new_value
 
     def __init_html(self):
         html = self.html
-        styles = self.styles
         pattern = 'class=\"(.[a-zA-Z-_ \d]+)\"'
         matches = re.finditer(pattern, html, flags=re.M)
-        tups_of_matches = [(match.group(), match.span()) for match in matches]
+        matches = [(match.group(), match.span()) for match in matches]
 
-        return tups_of_matches, pattern, html
+        return matches, pattern, html
 
-    def __get_new_style_value(self, value):
+    def __init_css(self):
+        css = self.css
+        pattern = r"""\.\D[^]['?#\s\r\n,{};:/\/\().]+"""
+        matches = re.finditer(pattern, css, flags=re.M)
+        matches = [(match.group(), match.span()) for match in matches]
+
+        return matches, css
+
+    def __get_new_style(self, value):
         if self.suffix not in value:
             value = self.styles.get(value)
             return value
@@ -89,7 +99,7 @@ class Refactor:
                 for index3, style in enumerate(self.styles.keys()):
                     if style == class_name and self.suffix not in class_name:
                         temp_line = re.sub('[^\-]'+class_name,
-                                           " "+self.__get_new_style_value(style), temp_line, count=1)
+                                           " "+self.__get_new_style(style), temp_line, count=1)
                     else:
                         continue
 
@@ -104,6 +114,31 @@ class Refactor:
 
         return file_string
 
+    def __unpack_css(self, matches, file_string):
+        # regex pattern playground -->  https://regex101.com/r/fvGD8T/3
+
+        pattern = r"""\.(?!png|svg|com|url|w3|ttf|org|gif|jpg|jpeg|onlinewebfonts|eot|woff)([a-z-_]+[^-.,)(\/])"""
+        for index, line in enumerate(file_string.splitlines()):
+            new_line = line
+            matches = re.finditer(
+                pattern, line, flags=re.MULTILINE)
+            if matches:
+                for match in matches:
+                    start, end = match.span()
+                    match = match.group()
+                    if match in self.styles:
+                        new_line = re.sub(
+                            match, self.styles[match], line, count=1, flags=re.MULTILINE)
+                        file_string = re.sub(
+                            line, new_line, file_string, count=1, flags=re.MULTILINE)
+                    else:
+                        continue
+
+            else:
+                continue
+
+        return file_string
+
     # =============================== USER METHODS ===============================
 
     def load_styles(self):
@@ -114,34 +149,34 @@ class Refactor:
     def pp_styles(self):
         print(*self.styles.items(), sep='\n')
 
-    def write(self, data=None):
-        if data == None:
-            data = self.__replace_html(self.styles, self.html)
-        else:
-            data = data
-        with open('refactor.html', 'w') as fp:
+    def write(self, data=None, filename=None):
+        if filename is None:
+            filename = input('ENTER FILENAME: ')
+
+        with open(filename, 'w') as fp:
             fp.write(data)
 
-        print(f"FILES REFACTORED WITH SUFFIX: '{self.suffix}'")
+        print(f"FILES REFACTORED WITH SUFFIX: '{self.suffix}' TO '{filename}'")
 
     # =============================== SANDBOX ===============================
 
-    def re_html(self, html):
+    def re_html(self):
         matches, pattern, html = self.__init_html()
         refactored_html = self.__unpack_matches(matches, pattern, html)
-        self.write(refactored_html)
+        self.write(refactored_html, 'RF.html')
 
-    # def re_css(self, css):
-    #     css = self.css
-    #     pattern = r"""\.\D[^]['?#\s\r\n,{};:/\/\().]+"""
-    #     for line in css.splitlines():
-    #         print(line)
+    def re_css(self):
+        matches, css = self.__init_css()
+        refactored_css = self.__unpack_css(matches, css)
+        print(refactored_css)
+        self.write(refactored_css, 'RF.css')
 
 
 if __name__ == "__main__":
     rf = Refactor()
     rf.load_styles()
+    # rf.re_html()
+    rf.re_css()
     html = rf.html
-    rf.re_html(html)
     styles = rf.styles
     css = rf.css
